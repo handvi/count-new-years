8df132ba4ce1817968b3e1bd240b8f8e9655b88c
Fhanafi <faisalhanafi2806@gmail.com>
2024-05-31 19:11:17 +0700

    fix style and js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 function updateCountdown() {
     const now = new Date();
     const currentYear = now.getFullYear();
-    const nextYear = now.getMonth() === 11 && now.getDate() > 31 ? currentYear + 1 : currentYear;
+    const nextYear = currentYear + 1;
     const newYear = new Date(`January 1, ${nextYear} 00:00:00`);
     const timeDifference = newYear - now;
 
@@ -12,10 +12,11 @@ function updateCountdown() {
         const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
 
         document.getElementById("countdown").innerHTML =
-            `${days}d ${hours}h ${minutes}m ${seconds}s `;
+            `${days}d ${hours}h ${minutes}m ${seconds}s`;
     } else {
         document.getElementById("countdown").innerHTML = "Happy New Year!";
     }
 }
 
 setInterval(updateCountdown, 1000);
+updateCountdown();
